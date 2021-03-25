bc7cb0113abab33320701fc8052c8ea566386b75
ilan.guenet <ilan.guenet@epita.fr>
2021-03-25 22:19:16 +0100

    up: custom optional

--- src/main.cu
+++ src/main.cu
@@ -1,4 +1,5 @@
 #include "cuda_tools/cuda_error.hh"
+#include "cuda_tools/optional.hh"
 #include "cuda_tools/vector.hh"
 int main()
 {
@@ -8,5 +9,10 @@ int main()
 
     cuda_safe_call(cudaDeviceSynchronize());
     check_error();
+
+    cuda_tools::Optional<int> a = cuda_tools::nullopt{};
+    std::cout << std::boolalpha << a.has_value() << std::endl;
+    a = 2;
+    std::cout << std::boolalpha << a.has_value() << ' ' << *a << std::endl;
     return 1;
 }
