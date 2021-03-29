919db97be069e5caf9f1526e6696bf80537b5abb
ilan.guenet <ilan.guenet@epita.fr>
2021-03-29 21:25:02 +0200

    up: light

--- src/scene/light.cuh
+++ src/scene/light.cuh
@@ -4,5 +4,22 @@ namespace scene
 {
 class Light
 {
+  public:
+    Light(const space::Point3& origin, const float intensity)
+        : origin_(origin)
+        , intensity_(intensity)
+    {
+    }
+
+    virtual ~Light() = default;
+    Light(const Light&) = default;
+    virtual Light& operator=(const Light&) = default;
+
+    float intensity_get() const { return intensity_; }
+    const space::Point3& origin_get() const { return origin_; }
+
+  protected:
+    const space::Point3 origin_;
+    const float intensity_;
 };
 } // namespace scene
