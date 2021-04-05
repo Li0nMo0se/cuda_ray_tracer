1ac35f5ffb6a41fee1d0fc3f488a8165c04e0669
ilan.guenet <ilan.guenet@epita.fr>
2021-04-05 20:46:29 +0200

    fix(image): copy image from host/device. Memory is not intialized

--- src/rendering/image.cuh
+++ src/rendering/image.cuh
@@ -23,6 +23,9 @@ class Image
     __host__ virtual void save(const std::string& filename) const = 0;
 
   protected:
+    __host__ void copy(const Image<T>& copy_img, cudaMemcpyKind memcpy_kind);
+
+  protected:
     uint32_t width_;
     uint32_t height_;
     T* data_ = nullptr;
@@ -40,21 +43,18 @@ template <typename T>
 class DeviceImage final : public Image<T>
 {
   public:
+    // Allocate uninitialize memory
     __host__ DeviceImage(const uint32_t width, const uint32_t height);
+
+    /* Copy */
+    __host__ DeviceImage(const DeviceImage& copy_device);
+    __host__ DeviceImage(const HostImage<T>& copy_host);
+    __host__ DeviceImage& operator=(const DeviceImage& copy_device);
+    __host__ DeviceImage& operator=(const HostImage<T>& copy_host);
 
     __host__ ~DeviceImage();
 
-    // FIXME:
-    // __host__ DeviceImage(const DeviceImage<T>& copy_device);
-    // __host__ DeviceImage& operator=(const DeviceImage<T>& copy_device);
-
-    __host__ DeviceImage(const HostImage<T>& copy_host);
-    __host__ DeviceImage& operator=(const HostImage<T>& copy_host);
-
     __host__ void save(const std::string& filename) const override;
-
-  private:
-    __host__ void copy(const Image<T>& copy_img, cudaMemcpyKind memcpy_kind);
 };
 
 /* A host Image can be instatiated by the host (The memory will be in
@@ -63,13 +63,19 @@ class DeviceImage final : public Image<T>
 template <typename T>
 class HostImage final : public Image<T>
 {
+  public:
+    // Allocate uninitialize memory
     __host__ HostImage(const uint32_t width, const uint32_t height);
+
+    /* Copy */
+    __host__ HostImage(const HostImage& copy_host);
+    __host__ HostImage(const DeviceImage<T>& copy_device);
+    __host__ HostImage& operator=(const HostImage& copy_host);
+    __host__ HostImage& operator=(const DeviceImage<T>& copy_device);
 
     __host__ ~HostImage();
 
-    // FIXME: Imagecopying
-
-    __host__ void save(const std::string filename) const override;
+    __host__ void save(const std::string& filename) const override;
 };
 } // namespace rendering
 
