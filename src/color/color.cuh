40b1ff3be6eac6fa14861ada14b45adb9e23cc4a
ilan.guenet <ilan.guenet@epita.fr>
2021-04-06 15:51:21 +0200

    up(color): black, white and background device constants

--- src/color/color.cuh
+++ src/color/color.cuh
@@ -6,6 +6,8 @@ namespace color
 {
 using Color3 = space::Vector<3, float>;
 
-const Color3 black = Color3(0.f, 0.f, 0.f);
-const Color3 white = Color3(255.f, 255.f, 255.f);
+// FIXME: Make those variables accessible from the device
+__device__ constexpr Color3 black() { return Color3(0.f, 0.f, 0.f); }
+__device__ constexpr Color3 white() { return Color3(255.f, 255.f, 255.f); }
+__device__ constexpr Color3 background() { return black(); }
 } // namespace color
